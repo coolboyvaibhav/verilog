09feaa6438c0424eaac358acb9e03cb10a2097d5
vaib <vaibhav.shatalwar07@gmail.com>
2023-11-26 08:52:36 +0530

    some modifications

--- project/eight_fft_main.v
+++ project/eight_fft_main.v
@@ -1,6 +1,4 @@
-// `include "dftf.v"
-// `include "twos_complement.v"
-// `include "inv_twos_complement.v"
+
 
 /////////////////////////twos_complement
 
@@ -93,39 +91,39 @@ wire [15:0] c1_r, c2_r, c3_r, c4_r, c5_r, c6_r, c7_r, c8_r, c1_i, c2_i, c3_i, c4
 reg signed [15:0] a1_r, a2_r, a3_r, a4_r, a5_r, a6_r, a7_r, a8_r, a1_i, a2_i, a3_i, a4_i, a5_i, a6_i, a7_i, a8_i;
 reg signed [15:0] b1_r, b2_r, b3_r, b4_r, b5_r, b6_r, b7_r, b8_r, b1_i, b2_i, b3_i, b4_i, b5_i, b6_i, b7_i, b8_i;
 
-two_complement p0(in0_real,t1_r);
-two_complement p1(in2_real,t2_r);
-two_complement p2(in3_real,t3_r);
-two_complement p3(in4_real,t4_r);
-two_complement p4(in5_real,t5_r);
-two_complement p5(in6_real,t6_r);
-two_complement p6(in7_real,t7_r);
-two_complement p7(in8_real,t8_r);
-two_complement p8(in0_imag,t1_i);
-two_complement p9(in2_imag,t2_i);
-two_complement p10(in3_imag,t3_i);
-two_complement p11(in4_imag,t4_i);
-two_complement p12(in5_imag,t5_i);
-two_complement p13(in6_imag,t6_i);
-two_complement p14(in7_imag,t7_i);
-two_complement p15(in8_imag,t8_i);
+two_complement two0(in0_real,t1_r);
+two_complement two1(in2_real,t2_r);
+two_complement two2(in3_real,t3_r);
+two_complement two3(in4_real,t4_r);
+two_complement two4(in5_real,t5_r);
+two_complement two5(in6_real,t6_r);
+two_complement two6(in7_real,t7_r);
+two_complement two7(in8_real,t8_r);
+two_complement two8(in0_imag,t1_i);
+two_complement two9(in2_imag,t2_i);
+two_complement two10(in3_imag,t3_i);
+two_complement two11(in4_imag,t4_i);
+two_complement two12(in5_imag,t5_i);
+two_complement two13(in6_imag,t6_i);
+two_complement two14(in7_imag,t7_i);
+two_complement two15(in8_imag,t8_i);
 
-inv_two po0(b1_r,c1_r);
-inv_two po1(b2_r,c2_r);
-inv_two po2(b3_r,c3_r);
-inv_two po3(b4_r,c4_r);
-inv_two po4(b5_r,c5_r);
-inv_two po5(b6_r,c6_r);
-inv_two po6(b7_r,c7_r);
-inv_two po7(b8_r,c8_r);
-inv_two po8(b1_i,c1_i);
-inv_two po9(b2_i,c2_i);
-inv_two po10(b3_i,c3_i);
-inv_two po11(b4_i,c4_i);
-inv_two po12(b5_i,c5_i);
-inv_two po13(b6_i,c6_i);
-inv_two po14(b7_i,c7_i);
-inv_two po15(b8_i,c8_i);
+inv_two invTwo0(b1_r,c1_r);
+inv_two invTwo1(b2_r,c2_r);
+inv_two invTwo2(b3_r,c3_r);
+inv_two invTwo3(b4_r,c4_r);
+inv_two invTwo4(b5_r,c5_r);
+inv_two invTwo5(b6_r,c6_r);
+inv_two invTwo6(b7_r,c7_r);
+inv_two invTwo7(b8_r,c8_r);
+inv_two invTwo8(b1_i,c1_i);
+inv_two invTwo9(b2_i,c2_i);
+inv_two invTwo10(b3_i,c3_i);
+inv_two invTwo11(b4_i,c4_i);
+inv_two invTwo12(b5_i,c5_i);
+inv_two invTwo13(b6_i,c6_i);
+inv_two invTwo14(b7_i,c7_i);
+inv_two invTwo15(b8_i,c8_i);
 
 dftf_module d0(a1_r,a1_i,a3_r,a3_i,a5_r,a5_i,a7_r,a7_i,y1_r,y1_i,y2_r,y2_i,y3_r,y3_i,y4_r,y4_i);
 dftf_module d1(a2_r,a2_i,a4_r,a4_i,a6_r,a6_i,a8_r,a8_i,y5_r,y5_i,y6_r,y6_i,y7_r,y7_i,y8_r,y8_i);
